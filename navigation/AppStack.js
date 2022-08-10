ef8d223d8f817c40767d5e733125ffb33821c522
wanderfulife <john.wander@hotmail.fr>
2022-08-11 08:42:26 +1000

    login

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -7,8 +7,8 @@ const Stack = createStackNavigator();
 
 export const AppStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
 };
